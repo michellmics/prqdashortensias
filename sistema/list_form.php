c0c0c5bbbc3e93f61bf56751a96f5fe9b15a53cb
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-17 22:28:26 +0000

    deploy

--- sistema/list_form.php
+++ sistema/list_form.php
@@ -167,12 +167,17 @@ html, body {
                     <div class="form-group has-warning">
                         <label class="control-label" for="inputWarning"><i class="fa fa-bell-o"></i> Nome Completo do Visitante</label>
                         <div class="form-row">
-                            <!-- Nome Completo do Visitante ocupa 8 partes da largura e Documento ocupa 4 partes -->
+                            <!-- Nome Completo do Visitante ocupa 8 partes da largura -->
                             <div class="col-8">
                                 <input id="nome" name="nome" style="text-transform: uppercase;" type="text" class="form-control" id="inputWarning" placeholder="Enter ..." required/>
                             </div>
-                            <div class="col-4">
+                            <!-- Documento ocupa 3 partes da largura -->
+                            <div class="col-3">
                                 <input id="documento" name="documento" style="text-transform: uppercase;" type="text" class="form-control" placeholder="Enter ..." maxlength="10" />
+                            </div>
+                            <!-- Botão ocupa 1 parte da largura -->
+                            <div class="col-1">
+                                <button type="button" class="btn btn-primary btn-block">Botão</button>
                             </div>
                         </div>
                     </div>
@@ -190,6 +195,7 @@ html, body {
         </div><!-- /.box -->
     </div><!--/.col (right) -->
 </section><!-- /.content -->
+
 
 
 
