f0ad8b10c4b01b525b6fae20d8e79ee8dcca8e01
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-18 19:00:45 +0000

    deploy

--- sistema/noAuth.php
+++ sistema/noAuth.php
@@ -203,7 +203,7 @@ html, body {
 	
 		<section class="content">
         <div class="error-page">
-          <h2 class="headline text-yellow"> 403</h2>
+          <h2 class="headline text-yellow"> </h2>
           <div class="error-content">
             <h3><i class="fa fa-warning text-yellow"></i> Permissões insuficientes.</h3>
             <p>
