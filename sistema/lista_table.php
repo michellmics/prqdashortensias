6d63be319e4b2130f8640c2409cd4974b66f1761
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-18 16:35:01 +0000

    deploy

--- sistema/lista_table.php
+++ sistema/lista_table.php
@@ -213,7 +213,7 @@ html, body {
 										<ul class="nav menu">
 											<li><a href="index.php">Inicio</a></li>
 											<li><a href="morador_form.php">Cadastro de Usuário </a></li>
-											<li><a href="lista_form.php">Salão de Festas </a></li>
+											<li><a href="#">Salão de Festas </a></li>
 											<li><a href="../logoff.php">Sair </a></li>
 										</ul>
 									</nav>
