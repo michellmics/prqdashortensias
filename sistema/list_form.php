134c1ad9278687ce4fec1da0a6dea6cb27753ad0
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-18 01:05:20 +0000

    deploy

--- sistema/list_form.php
+++ sistema/list_form.php
@@ -172,7 +172,7 @@ html, body {
                         <div class="form-row">
                             <!-- Nome Completo do Visitante ocupa 8 partes da largura e Documento ocupa 4 partes -->
                             <div class="col-8">
-								<label class="control-label" for="inputWarning"><i class="fa fa-bell-o"></i> Nome Completo do Visitante</label>
+								<label class="control-label" for="inputWarning"><i class="fa fa-bell-o"></i> Nome Completo</label>
                                 <input id="nome" name="nome" style="text-transform: uppercase;" type="text" class="form-control" id="inputWarning" placeholder="NOME COMPLETO..." maxlength="28" required/>
                             </div>
                             <div class="col-4">
