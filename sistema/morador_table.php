b49edccf33f01d74922d94f954f3f093cc5e5db3
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-19 01:01:25 +0000

    deploy

--- sistema/morador_table.php
+++ sistema/morador_table.php
@@ -426,24 +426,6 @@ html, body {
     document.getElementById('form-empresa').addEventListener('submit', validarFormulario);
 </script>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <!-- Footer -->
 <footer class="footerNew">
 <a href="https://codemaze.com.br" target="_blank"><b>Codemaze</b></a> - Soluções de MKT e Software | <b><font color="red"><? echo $_SESSION['user_nivelacesso']; ?></font></b>
