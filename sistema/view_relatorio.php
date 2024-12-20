b70543be118d9178a7be19718d968bf02f3ffb60
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-20 00:49:09 +0000

    deploy

--- sistema/view_relatorio.php
+++ sistema/view_relatorio.php
@@ -75,6 +75,9 @@
 
 		<!-- Chart.js -->
 		<script src="vendors/Chart.js/dist/Chart.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    	<script src="vendors/echarts/dist/echarts.min.js"></script>
+    	<script src="vendors/echarts/map/js/world.js"></script>
 		<script src="codemaze_js\codemaze_js_custom.js"></script>
 
 		<!-- DASHBOARD -->
