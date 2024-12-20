a2dc8e763d8a59b8a3c3a054109ad4522681ed14
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-20 00:31:43 +0000

    deploy

--- sistema/view_relatorio.php
+++ sistema/view_relatorio.php
@@ -222,34 +222,6 @@ html, body {
 		<section class="content" style="display: flex; justify-content: center; align-items: center; height: 100vh;">
     		
 
-  	    <!-- INI GRAFICO PIZZA DETALHE -->
-		<div class="col-md-4 col-sm-4  ">
-          <div class="x_panel">
-            <div class="x_title">
-              <h2>Pie Graph</h2>
-              <ul class="panel_toolbox">
-                <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                </li>
-                <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                      <a class="dropdown-item" href="#">Settings 1</a>
-                      <a class="dropdown-item" href="#">Settings 2</a>
-                    </div>
-                </li>
-                <li><a class="close-link"><i class="fa fa-close"></i></a>
-                </li>
-              </ul>
-              <div class="clearfix"></div>
-            </div>
-            <div class="x_content">
-
-              <div id="echart_pie" style="height:350px;"></div>
-
-            </div>
-          </div>
-        </div>
-        <!-- FIM GRAFICO PIZZA DETALHE -->
 
 
 
