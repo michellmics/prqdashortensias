73c86d01f33e02ccd8e3096d3e5d7b40b5a3ce27
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-23 01:48:35 +0000

    deploy

---
+++ processa_recuperacao.php
@@ -0,0 +1,42 @@
+<?php
+include_once 'objetos.php'; 
+
+class RecSystem extends SITE_ADMIN
+{
+    public function CheckValidUser($apartamento, $password)
+    {
+            if (!$this->pdo) {
+                $this->conexao();
+            }
+
+            $sql = "SELECT USU_DCEMAIL FROM USU_USUARIO WHERE USU_DCAPARTAMENTO = :USU_DCAPARTAMENTO";
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->bindParam(':USU_DCAPARTAMENTO', $apartamento, PDO::PARAM_STR);
+            $stmt->execute();
+
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+            
+            if ($user['USU_DCEMAIL']) 
+            {
+                $email = $user['USU_DCEMAIL'];
+                return $email;             
+            } else 
+                {
+                    echo "Apartamento não cadastrado no sistema. Entre em contato com o síndico.";
+                    exit();
+                }
+        
+    }
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') 
+{
+    $apartamento = $_POST['apartamento'];
+
+    $RecSystem = new RecSystem();
+    
+    $result=$RecSystem->CheckValidUser($apartamento);
+
+}
+ 
+?>
--- recuperar_senha.php
+++ recuperar_senha.php
@@ -45,7 +45,6 @@
 
 						<div id="form-message"></div>
 
-
 						<!-- Ajax para envio e exibicao do resultado sem load de pag nova -->
 						<script>
 							document.getElementById('demo-form').addEventListener('submit', function(e) {
@@ -68,18 +67,13 @@
 							    xhr.send(formData);
 							});
 						</script>
-
-       
-
       </div><!-- /.login-box-body -->
     </div><!-- /.login-box -->
-
     <!-- jQuery 2.1.3 -->
     <script src="plugins/jQuery/jQuery-2.1.3.min.js"></script>
     <!-- Bootstrap 3.3.2 JS -->
     <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
     <!-- iCheck -->
     <script src="plugins/iCheck/icheck.min.js" type="text/javascript"></script>
-
   </body>
 </html>
