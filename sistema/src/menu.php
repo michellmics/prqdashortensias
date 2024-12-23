8be8f33f372f2f42667946a79cc21324ecb39c10
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-23 11:39:06 +0000

    deploy

--- sistema/src/menu.php
+++ sistema/src/menu.php
@@ -4,6 +4,7 @@
 		<li><a href="morador_table.php">Moradores </a></li>
 		<li><a href="lista_table.php">Minha Lista de Convidados </a></li>
 		<li><a href="morador_form_edit_profile.php">Minha Conta </a></li> 
+        <li><a href="https://prqdashortensias.com.br/webmail" target="_blank">WebMail </a></li> 
 		<li><a href="../logoff.php">Sair </a></li>
 	</ul>
 </nav>
