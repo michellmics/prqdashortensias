2b31386e947fe84f452bafe00da9c6144bc81725
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-23 11:56:56 +0000

    deploy

--- sistema/src/menu.php
+++ sistema/src/menu.php
@@ -64,11 +64,9 @@
 
 </style>
 
-<nav class="">
+<nav class="navigation">
     <ul class="nav menu">
         <li><a href="index.php">Inicio</a></li>
-        <li><a href="lista_table.php">Minha Lista de Convidados</a></li>
-        <li><a href="morador_form_edit_profile.php">Minha Conta</a></li>
         <li>
             <a href="#">Administração</a>
             <ul class="submenu">
@@ -76,6 +74,8 @@
                 <li><a href="https://prqdashortensias.com.br/webmail" target="_blank">WebMail</a></li>
             </ul>
         </li>
+        <li><a href="lista_table.php">Minha Lista de Convidados</a></li>
+        <li><a href="morador_form_edit_profile.php">Minha Conta</a></li>
         <li><a href="../logoff.php">Sair</a></li>
     </ul>
 </nav>
