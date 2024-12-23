eed509453b13db6cc23e2cb4aba6a852c226f1fb
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-23 11:49:17 +0000

    deploy

--- sistema/src/menu.php
+++ sistema/src/menu.php
@@ -49,12 +49,12 @@
 <nav class="navigation">
     <ul class="nav menu">
         <li><a href="index.php">Inicio</a></li>
-        <li><a href="morador_table.php">Moradores</a></li>
         <li><a href="lista_table.php">Minha Lista de Convidados</a></li>
         <li><a href="morador_form_edit_profile.php">Minha Conta</a></li>
         <li>
             <a href="#">Administração</a>
             <ul class="submenu">
+                <li><a href="morador_table.php">Moradores</a></li>
                 <li><a href="https://prqdashortensias.com.br/webmail" target="_blank">WebMail</a></li>
             </ul>
         </li>
