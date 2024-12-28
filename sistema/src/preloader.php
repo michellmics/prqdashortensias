d7a0f7df71ff9f5df6f7f4d00f69cb0095f353f3
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-28 16:59:49 +0000

    deploy

---
+++ sistema/src/preloader.php
@@ -0,0 +1,16 @@
+		<!-- Preloader -->
+		<div class="preloader">
+		    <div class="loader">
+		        <div class="loader-outter"></div>
+		        <div class="loader-inner"></div>
+
+		        <div class="indicator"> 
+		            <!-- Substitua este SVG pelo seu novo ícone -->
+		            <svg width="50px" height="50px" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+		                <circle cx="50" cy="50" r="45" stroke="#000" stroke-width="5" fill="none" />
+		                <circle cx="50" cy="50" r="40" stroke="#3498db" stroke-width="5" fill="none" />
+		            </svg>
+		        </div>
+		    </div>
+		</div>
+		<!-- End Preloader -->
