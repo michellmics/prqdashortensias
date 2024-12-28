b84c863a15f1795a33051f99c5eca53a6e3abd68
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-28 11:43:19 +0000

    deploy

--- sistema/lista_log.php
+++ sistema/lista_log.php
@@ -252,7 +252,7 @@ html, body {
                       <tr>
                         <td style="text-transform: uppercase; font-size: 10px; vertical-align: middle; color:#993399; white-space: nowrap; ?>"> <?= htmlspecialchars(strlen($log['LOG_DTLOG']) > 20 ? substr($log['LOG_DTLOG'], 0, 20) . '...' : $log['LOG_DTLOG']) ?></td>                        
                         <td style="text-transform: uppercase; font-size: 10px; vertical-align: middle; color:#993399; ?>"><?= htmlspecialchars(strlen($log['LOG_DCAPARTAMENTO']) > 25 ? substr($log['LOG_DCAPARTAMENTO'], 0, 12) . '...' : $log['LOG_DCUSUARIO']) ?></td> 
-                        <td style="text-transform: uppercase; font-size: 10px; vertical-align: middle; color:#993399; ?>"><?= htmlspecialchars(strlen($log['LOG_DCMSG']) > 50 ? substr($log['LOG_DCMSG'], 0, 80) . '...' : $log['LOG_DCMSG']) ?></td> 
+                        <td style="text-transform: uppercase; font-size: 10px; vertical-align: middle; color:#993399; ?>"><?= htmlspecialchars(strlen($log['LOG_DCMSG']) > 100 ? substr($log['LOG_DCMSG'], 0, 100) . '...' : $log['LOG_DCMSG']) ?></td> 
 						      
                       
                     <?php endforeach; ?>   
