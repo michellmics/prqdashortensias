1a4d71eb831249afb73dfc492ec071a81cb93c1c
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-28 23:01:59 +0000

    deploy

--- sistema/view_relatorio.php
+++ sistema/view_relatorio.php
@@ -281,24 +281,7 @@ html, body {
 					<!-- INI TABELA -->
 					<div class="col-md-12 col-sm-12 ">
         			  <div class="x_panel">FFF
-        			    <div class="x_title">
-        			      <h2>Button Example <small>Users</small></h2>
-        			      <ul class="nav navbar-right panel_toolbox">
-        			        <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-        			        </li>
-        			        <li class="dropdown">
-        			          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-        			          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        			              <a class="dropdown-item" href="#">Settings 1</a>
-        			              <a class="dropdown-item" href="#">Settings 2</a>
-        			            </div>
-        			        </li>
-        			        <li><a class="close-link"><i class="fa fa-close"></i></a>
-        			        </li>
-        			      </ul>
-        			      <div class="clearfix"></div>
-        			    </div>
-						<div class="x_content table-responsive">
+        						<div class="x_content table-responsive">
         			        <div class="row">
         			            <div class="col-sm-12">
         			              <div class="card-box table-responsive">
