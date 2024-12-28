48a4477ef828682a6d376cd4f95a1287c9057677
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-28 11:13:30 +0000

    deploy

--- sistema/lista_form.php
+++ sistema/lista_form.php
@@ -193,11 +193,7 @@ html, body {
 							<div class="col-lg-7 col-md-9 col-12">
 								<!-- Main Menu -->
 								<div class="main-menu">
-									<nav class="navigation">
-										<ul class="nav menu">
-                                            <?php include 'src/menu.php'; ?>
-										</ul>
-									</nav>
+                  <?php include 'src/menu.php'; ?>
 								</div>
 								<!--/ End Main Menu -->
 							</div>
