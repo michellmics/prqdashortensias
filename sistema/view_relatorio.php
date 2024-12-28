2bf8f5b5e60cb06cca8f503c8d1952422dc818d1
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-28 17:04:28 +0000

    deploy

--- sistema/view_relatorio.php
+++ sistema/view_relatorio.php
@@ -172,60 +172,7 @@ html, body {
      
 
 		<!-- Header Area -->
-		<header class="header" >
-			<!-- Topbar -->
-			<div class="topbar">
-				<div class="container">
-					<div class="row">
-						<div class="col-lg-6 col-md-5 col-12">
-							<!-- Contact -->
-							<ul class="top-link">
-								<!-- <li><a href="#">Serviços</a></li> -->
-								<!-- <li><a href="#">Contact</a></li> -->
-								<!-- <li><a href="#">FAQ</a></li> -->
-							</ul>
-							<!-- End Contact -->
-						</div>
-						<div class="col-lg-6 col-md-7 col-12">
-							<!-- Top Contact -->
-							<ul class="top-contact">
-								<li><b>Morador:</b> <? echo $usuariologado; ?></li> 
-								<!--  <li><i class="fa fa-envelope"></i><a href="mailto:sada@sdf.com">23123213123</a></li> -->
-							</ul>
-							<!-- End Top Contact -->
-						</div>
-					</div>
-				</div>
-			</div>
-			<!-- End Topbar -->
-			<!-- Header Inner -->
-			<div class="header-inner">
-				<div class="container">
-					<div class="inner">
-						<div class="row">
-							<div class="col-lg-2 col-md-3 col-12">
-								<!-- Start Logo -->
-								<div class="logo">
-									<a href="#slider"><img src="https://prqdashortensias.com.br/sistema/img/logo_hor_hort.png" alt="#"></a>
-								</div>
-								<!-- End Logo -->
-								<!-- Mobile Nav -->
-								<div class="mobile-nav"></div>
-								<!-- End Mobile Nav -->
-							</div>
-							<div class="col-lg-10 col-md-9 col-12">
-								<!-- Main Menu -->
-								<div class="main-menu">
-									<?php include 'src/menu.php'; ?>
-								</div>
-								<!--/ End Main Menu -->
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<!--/ End Header Inner -->
-		</header>
+		<?php include 'src/header.php'; ?>
 		<!-- End Header Area -->
 		
   		<!-- ini content Area -->
