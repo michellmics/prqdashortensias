c7e9eaf99632f65c2ac31296833f1123db388159
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-28 13:26:21 +0000

    deploy

--- sistema/index.php
+++ sistema/index.php
@@ -195,7 +195,7 @@ html, body {
 		
 	
 		<section class="content" style="display: flex; justify-content: center; align-items: center; height: 100vh;">
-    		<img src="https://prqdashortensias.com.br/sistema/img/logo_icon.png" alt="Logo Parque das Hortênsias" style="max-width: 100%; height: auto;">
+    		<img src="https://prqdashortensias.com.br/sistema/img/logo_icon_trasnparent.png" alt="Logo Parque das Hortênsias" style="max-width: 100%; height: auto;">
 		</section>
 
 
