e3a19ad9f2ae54fd384d4b3c5554f54712f00bfe
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-28 13:05:12 +0000

    deploy

--- sistema/lista_delete.php
+++ sistema/lista_delete.php
@@ -31,7 +31,7 @@ class deleteLista extends SITE_ADMIN
             $LOG_DCTIPO = "REMOÇÃO DE VISITANTE";
             $LOG_DCMSG = "O visitante com id $LIS_IDLISTACONVIDADOS foi removido com sucesso.";
             $LOG_DCUSUARIO = $_SESSION['user_id'];
-            $LOG_DCAPARTAMENTO = "N/A";
+            $LOG_DCAPARTAMENTO = $_SESSION['user_apartamento'];
             $this->insertLogInfo($LOG_DCTIPO, $LOG_DCMSG, $LOG_DCUSUARIO, $LOG_DCAPARTAMENTO);
             //--------------------LOG----------------------//
 
