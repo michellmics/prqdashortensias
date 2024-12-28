f2ccd8ccde8d3947d66baabcac2f156c0ade4944
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-28 18:11:50 +0000

    deploy

--- sistema/view_relatorio.php
+++ sistema/view_relatorio.php
@@ -221,7 +221,7 @@ html, body {
 					<div class="col-md-6 col-sm-12" style="border: 0px solid #d4cccb;">
         			  <div class="x_panel">
         			    <div class="x_title">
-        			      <h1>Receita (Mês Corrente)</h1>
+        			      <h1>Receitas</h1>
         			      <ul class="nav navbar-right panel_toolbox">
         			        <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
         			        </li>
