7823947f4723362e2198fec12de82ede9cde98ad
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-28 22:39:56 +0000

    deploy

--- sistema/codemaze_js/codemaze_js_custom.js
+++ sistema/codemaze_js/codemaze_js_custom.js
@@ -2405,8 +2405,8 @@ function init_echarts() {
 							color: [
                                 [.2, "#9b59b6"], // 0% a 20% Roxo
                                 [.4, "#34495e"] // 20% a 40% chumbo
-                              //  [.8, "#bdc3c7"], // 40% a 80% cinza
-                               // [1, "#26b99a"]    // 80% a 100% verde
+                                [.8, "#bdc3c7"] // 40% a 80% cinza
+                                //[1, "#26b99a"]    // 80% a 100% verde
 							],
 							width: 30
 						}
