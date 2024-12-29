a5060d0fec1e318de65bb41c7e615ba79cd32a22
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-29 13:20:18 +0000

    deploy

--- sistema/codemaze_js/codemaze_js_custom.js
+++ sistema/codemaze_js/codemaze_js_custom.js
@@ -4359,7 +4359,7 @@ $.fn.popover.Constructor.prototype.leave = function(e) {
 
 // Função para buscar dados de uma consulta específica
 function buscarDados(consulta) {
-    fetch('charts_consultas.php', {
+    fetch('codemaze_js/charts_consultas.php', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
