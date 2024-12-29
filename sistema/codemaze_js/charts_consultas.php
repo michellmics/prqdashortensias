0e854e65121a7cf98427710825faac330a2f5efe
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-29 12:33:59 +0000

    deploy

---
+++ sistema/codemaze_js/charts_consultas.php
@@ -0,0 +1,16 @@
+<?php
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+
+  include_once '../../objetos_chart.php'; 
+	$siteCharts = new SITE_CHARTS();    
+
+  $despesas = $siteCharts->getDespesasFull();
+  echo json_encode($despesas);
+
+
+
+
+?>
+
