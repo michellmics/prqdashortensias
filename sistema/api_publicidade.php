56761fae7579aa54264590ca9ba60221c26a73f7
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-31 13:33:06 +0000

    deploy

--- sistema/api_publicidade.php
+++ sistema/api_publicidade.php
@@ -14,8 +14,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $mktId = $_POST['mktId'] ?? null;
     $imagem="";
 
-    var_dump($dataInicio);
-
     $publi = new SITE_ADMIN();
 
     if (!$dataInicio || !$dataFim || !$clienteOrigin || !$status || !$mktId) {
