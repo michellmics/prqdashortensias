7e15087893c477cc499c5ef93a997a99953bfe43
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-31 03:09:27 +0000

    deploy

---
+++ sistema/api_publicidade.php
@@ -0,0 +1,8 @@
+<?php
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
+include_once '../objetos.php'; // Carrega a classe de conexão e objetos
+
+
+
