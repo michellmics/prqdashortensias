f3eeddc75b3463c045a4a9d4f4fa675ec2233e53
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-31 02:51:09 +0000

    deploy

--- sistema/src/footer.php
+++ sistema/src/footer.php
@@ -46,7 +46,7 @@
 <footer class="footerNew">
   <!-- Marquee com o conteúdo rolante -->
   <div class="marquee-container">
-    <span>adsdasasdgasasjj</span>
+    <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat</span>
   </div>
   
   <!-- Conteúdo abaixo do marquee -->
