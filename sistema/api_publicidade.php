532180e4b6e0365fe7d972b871a88674574fded4
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-31 12:00:44 +0000

    deploy

--- sistema/api_publicidade.php
+++ sistema/api_publicidade.php
@@ -8,31 +8,42 @@ include_once '../objetos.php'; // Carrega a classe de conexão e objetos
 
 
 // Verifica se um arquivo foi enviado via POST
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['file'])) {
-    $file = $_FILES['file'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    // Verifica se houve erro no upload
-    if ($file['error'] === UPLOAD_ERR_OK) {
-        // Define o diretório de upload
-        $uploadDirectory = 'uploads/';
-        if (!is_dir($uploadDirectory)) {
-            mkdir($uploadDirectory, 0777, true); // Cria o diretório se não existir
-        }
 
-        // Define o caminho para salvar a imagem
-        $uploadPath = $uploadDirectory . basename($file['name']);
+    if(isset($_FILES['file']))
+    {
+        $file = $_FILES['file'];
 
-        // Move a imagem para o diretório de uploads
-        if (move_uploaded_file($file['tmp_name'], $uploadPath)) {
-            // Retorna um JSON com sucesso e o caminho da imagem
-            echo json_encode(['success' => true, 'path' => $uploadPath]);
+        // Verifica se houve erro no upload
+        if ($file['error'] === UPLOAD_ERR_OK) {
+            // Define o diretório de upload
+            $uploadDirectory = 'uploads/';
+            if (!is_dir($uploadDirectory)) {
+                mkdir($uploadDirectory, 0777, true); // Cria o diretório se não existir
+            }
+    
+            // Define o caminho para salvar a imagem
+            $uploadPath = $uploadDirectory . basename($file['name']);
+    
+            // Move a imagem para o diretório de uploads
+            if (move_uploaded_file($file['tmp_name'], $uploadPath)) {
+                // Retorna um JSON com sucesso e o caminho da imagem
+                echo json_encode(['success' => true, 'path' => $uploadPath]);
+            } else {
+                echo json_encode(['success' => false, 'message' => 'Erro ao mover o arquivo']);
+            }
         } else {
-            echo json_encode(['success' => false, 'message' => 'Erro ao mover o arquivo']);
+            echo json_encode(['success' => false, 'message' => 'Erro no upload da imagem']);
         }
-    } else {
-        echo json_encode(['success' => false, 'message' => 'Erro no upload da imagem']);
+        } else {
+            echo json_encode(['success' => false, 'message' => 'Nenhum arquivo enviado']);
+        }
     }
-} else {
-    echo json_encode(['success' => false, 'message' => 'Nenhum arquivo enviado']);
-}
+
+    
+
+
+
+
 ?>
