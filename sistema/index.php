82ecfc841f53682c5ea7ac02d1064bff1dd0acb2
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-31 02:22:37 +0000

    deploy

--- sistema/index.php
+++ sistema/index.php
@@ -132,7 +132,7 @@ html, body {
 
     .popup-content {
         position: relative;
-        background: white;
+		background: transparent; /* Alterado para transparente */
         padding: 20px;
         border-radius: 10px;
         box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
