1c1837f8a41cf48feaf132851efefd3dac800094
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-31 02:34:33 +0000

    deploy

--- sistema/src/footer.php
+++ sistema/src/footer.php
@@ -1,6 +1,7 @@
-<footer class="footerNew">
+<footer class="footerNew"><br> huhusahduiasd
+sdfdsfdsfdsfds<br>sdfdsfsdf
   <a href="https://codemaze.com.br" target="_blank"><b>Codemaze</b></a> - Soluções de Mkt e Software | 
   <b><font color="red"><?php echo $_SESSION['user_nivelacesso']; ?></font></b> | <a href="https://prqdashortensias.com.br/webmail" target="_blank">Webmail</a>
-  <br> huhusahduiasd
+  
 
 </footer>
