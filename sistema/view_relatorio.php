10130411135c07dad74880b9dc1d9688d3b0fc0e
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-02 21:15:08 +0000

    deploy

--- sistema/view_relatorio.php
+++ sistema/view_relatorio.php
@@ -243,6 +243,18 @@ html, body {
         			    <div class="x_content">
 
         			      <div id="echart_pie_codemaze" data-valor=<? echo $dataValor; ?> style="height:350px;"></div>
+						              <!-- INI EXPLICAÇÃO -->
+									  <div style="margin-top: 20px;">
+              <div class="x_panel">
+                <div class="x_content">
+                  <p>
+                    <strong>Gráfico 'Receitas (Top 10)'</strong><br>
+                    O objetivo deste gráfico é apresentar as 10 maiores receitas conforme o mês selecionado. Os valores exibidos estão em Reais (R$) e podem ser ajustados à medida que os moradores efetuam os pagamentos nos meses subsequentes.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <!-- FIM EXPLICAÇÃO -->
 							
         			    </div>
 						
