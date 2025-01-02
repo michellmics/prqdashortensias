0922b1fd8eb779ec3af5151e2c5254eb97e4204c
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-02 20:23:31 +0000

    deploy

--- objetos_chart.php
+++ objetos_chart.php
@@ -86,7 +86,8 @@
                         AND CONC.CON_DCANO_COMPETENCIA_USUARIO = :CON_DCANO_COMPETENCIA_USUARIO
                         AND CONC.CON_DCMES_COMPETENCIA = :CON_DCMES_COMPETENCIA
                         GROUP BY 
-                        CONC.CON_NMTITULO";
+                        CONC.CON_NMTITULO
+                        ORDER BY TITULO DESC";
 
                 $stmt = $this->pdo->prepare($sql);
                 $stmt->bindParam(':CON_DCMES_COMPETENCIA_USUARIO', $CON_DCMES_COMPETENCIA_USUARIO, PDO::PARAM_STR);
