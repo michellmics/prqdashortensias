1e69bc5a478dbe15062dc2bdfc096c518a4a2c81
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-02 23:49:59 +0000

    deploy

--- sistema/view_relatorio.php
+++ sistema/view_relatorio.php
@@ -206,7 +206,7 @@ html, body {
 					
 				</div>
 				<div class="col-2">
-				    <label class="control-label" for=""></label>
+				    <label class="control-label" for="mesAno">&nbsp;</label>
 					<button 
             type="button" 
             class="btn btn-primary" 
