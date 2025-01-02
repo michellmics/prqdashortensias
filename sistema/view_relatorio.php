8f709ff6abe3f0e56234f3caa54f7b0776085b9f
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-02 20:39:23 +0000

    deploy

--- sistema/view_relatorio.php
+++ sistema/view_relatorio.php
@@ -240,7 +240,6 @@ html, body {
         			      </ul>
         			      <div class="clearfix"></div>
         			    </div>
-						<br>
         			    <div class="x_content">
 
         			      <div id="echart_pie_codemaze" data-valor=<? echo $dataValor; ?> style="height:350px;"></div>
@@ -249,6 +248,18 @@ html, body {
         			  </div>
         			</div>
         			<!-- FIM GRAFICO PIZZA DETALHE -->
+					 		<!-- INI EXPLICAÇÃO -->
+							<div class="col-md-6 col-sm-12" style="margin-top: 20px;">
+							    <div class="x_panel">
+							        <div class="x_content">
+							            <p>
+							                <strong>Explicação do gráfico:</strong><br>
+							                Este gráfico de pizza exibe as receitas dos 10 principais itens para o mês selecionado. Cada fatia representa uma categoria de receita, como "Multas", "Taxa Condominial" ou "Salao de Festa". A porcentagem de cada fatia é calculada com base no total das receitas e permite uma visualização rápida da contribuição de cada item para o total.
+							            </p>
+							        </div>
+							    </div>
+							</div>
+							<!-- FIM EXPLICAÇÃO -->
 				</div>
 				<div class="row">
         			<!-- INI GRAFICO AREA -->
