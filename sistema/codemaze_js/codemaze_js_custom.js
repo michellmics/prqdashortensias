a1b3928b03575c7db27c1e2244c134cd4d0247bc
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-02 19:23:44 +0000

    deploy

--- sistema/codemaze_js/codemaze_js_custom.js
+++ sistema/codemaze_js/codemaze_js_custom.js
@@ -3492,6 +3492,7 @@ function init_echarts() {
             
             async function carregarDadosPie() {
                 try {
+                    /*
                     // Supondo que você tenha uma função 'buscarDados' para buscar dados de inadimplência ou qualquer outro dado necessário
                     const dados = await buscarDados('receita', mes, ano);
                     const categorias = ["Multas", "Salão de Festas", "Processos", "Construtora", "Cota Condominal"];
@@ -3502,8 +3503,9 @@ function init_echarts() {
                         32, // valor da categoria "Construtora"
                         22 // valor da categoria "Cota Condominal"
                     ];
+                    */
         
-                    console.log('Valores recebidos:', valores);
+                    //console.log('Valores recebidos:', valores);
         
                     // Atualiza o gráfico com os dados obtidos
                     var option = {
