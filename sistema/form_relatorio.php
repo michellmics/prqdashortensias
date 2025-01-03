b7e0b28af53f6f1b98415de5cf0cea92b6cbabfd
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-03 00:14:27 +0000

    deploy

---
+++ sistema/form_relatorio.php
@@ -0,0 +1,251 @@
+<?php
+	include_once '../objetos.php'; // Carrega a classe de conexão e objetos
+	
+	session_start(); 
+	define('SESSION_TIMEOUT', 43200); // 30 minutos
+	
+	if (!isset($_SESSION['user_id'])) 
+	{
+	  header("Location: https://www.prqdashortensias.com.br/index.php");
+	  exit();
+	}
+    	// Atualiza o timestamp da última atividade
+	$_SESSION['last_activity'] = time();
+
+	if (!isset($_SESSION['user_id'])) 
+	{
+	  header("Location: https://www.prqdashortensias.com.br/index.php");
+	  exit();
+	}
+	
+	$blocoSession = $_SESSION['user_bloco'];
+	$apartamentoSession = $_SESSION['user_apartamento'];
+	$nomeSession =  ucwords($_SESSION['user_name']);
+	$usuariologado = $nomeSession." <b>BL</b> ".$blocoSession." <b>AP</b> ".$apartamentoSession;
+	$userid = $_SESSION['user_id'];
+
+    $siteAdmin = new SITE_ADMIN();  
+    $siteAdmin->getPopupImagePublish();    
+
+    //var_dump($siteAdmin->ARRAY_FOOTERPUBLISHINFO);
+
+    $qtdePubli = count($siteAdmin->ARRAY_POPUPPUBLISHINFO);
+    $num = rand(0, $qtdePubli -1);
+    $publiImage = "https://prqdashortensias.com.br/sistema/".$siteAdmin->ARRAY_POPUPPUBLISHINFO[$num]["PUB_DCIMG"];
+    
+    if($siteAdmin->ARRAY_POPUPPUBLISHINFO[$num]["PUB_DCLINK"] != "")
+    {
+        $publiImageLink = 'href="' . $siteAdmin->ARRAY_POPUPPUBLISHINFO[$num]["PUB_DCLINK"] . '" target="_blank"';
+    }
+    else
+        {
+            $publiImageLink = "";
+        }
+
+    
+
+
+?>
+
+<!doctype html>
+<html class="no-js" lang="zxx">
+    <head>
+        <!-- Meta Tags -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="keywords" content="Site keywords here">
+		<meta name="description" content="">
+		<meta name='copyright' content=''>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
+		<!-- Title -->
+        <title>Condomínio Parque das Hortências</title>
+		
+		<!-- Favicon -->
+        <link rel="icon" href="img/favicon.png">
+		
+		<!-- Google Fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<!-- Nice Select CSS -->
+		<link rel="stylesheet" href="css/nice-select.css">
+		<!-- Font Awesome CSS -->
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+		<!-- icofont CSS -->
+        <link rel="stylesheet" href="css/icofont.css">
+		<!-- Slicknav -->
+		<link rel="stylesheet" href="css/slicknav.min.css">
+		<!-- Owl Carousel CSS -->
+        <link rel="stylesheet" href="css/owl-carousel.css">
+		<!-- Datepicker CSS -->
+		<link rel="stylesheet" href="css/datepicker.css">
+		<!-- Animate CSS -->
+        <link rel="stylesheet" href="css/animate.min.css">
+		<!-- Magnific Popup CSS -->
+        <link rel="stylesheet" href="css/magnific-popup.css">
+		
+		<!-- Medipro CSS -->
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="css/responsive.css">
+
+        <!-- SWEETALERT -->
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+		<link rel="icon" href="https://www.prqdashortensias.com.br/logo_icon.ico" type="image/x-icon">
+    	<link rel="shortcut icon" href="https://www.prqdashortensias.com.br/logo_icon.ico" type="image/x-icon">
+    	<link rel="apple-touch-icon" href="https://www.prqdashortensias.com.br/logo_icon.png">
+    	<meta name="apple-mobile-web-app-title" content="Hortensias">
+    	<meta name="apple-mobile-web-app-capable" content="yes">
+    	<meta name="apple-mobile-web-app-status-bar-style" content="default">
+
+        <style>
+          /* Configuração geral do HTML e Body */
+html, body {
+    height: 100%;
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+}
+
+/* Footer fixo no final */
+.footerNew {
+    background-color: #f0f0f0; /* Cinza bem claro */
+    color: #000; /* Cinza clarinho para o texto */
+    font-size: 12px; /* Fonte pequena */
+    text-align: center; /* Centraliza o texto */
+    padding: 0; /* Remove espaçamento interno */
+    height: 65px; /* Altura do footer */
+    line-height: 30px; /* Centraliza verticalmente o texto */
+    position: fixed; /* Fixa o footer */
+    bottom: 0; /* Colado no final da página */
+    width: 100%; /* Largura total */
+}
+ /* Botão de Salvar em lilás */
+ .btn-primary {
+      background-color:#8a0653; /* Lilás */
+      border-color: #8a0653; /* Mesma cor da borda */
+      color: white; /* Texto branco */
+  }
+  .btn-primary:hover {
+      background-color: #993399; /* Lilás mais claro ao passar o mouse */
+  }
+
+  /* Botão de Voltar em lilás mais claro */
+  .btn-warning {
+      background-color: #D8BFD8; /* Lilás mais claro */
+      border-color: #D8BFD8; /* Mesma cor da borda */
+      color: #4B0082; /* Texto em roxo escuro */
+  }
+  .btn-warning:hover {
+      background-color: #E6E6FA; /* Lilás ainda mais claro ao passar o mouse */
+  }
+
+</style>
+
+	
+    </head>
+    <body>
+	
+		<!-- Preloader -->
+		<?php include 'src/preloader.php'; ?>
+		<!-- End Preloader -->
+     
+
+		<!-- Header Area -->
+		<?php include 'src/header.php'; ?>
+		<!-- End Header Area -->
+
+
+
+	
+
+
+
+		<section class="content" style="display: flex; justify-content: center; align-items: center; height: 100vh;">
+    		<img src="https://prqdashortensias.com.br/sistema/img/logo_icon_trasnparent.png" alt="Logo Parque das Hortênsias" style="max-width: 100%; height: auto;">
+		</section>
+
+
+
+<!-- Footer -->
+<footer class="footerNew">
+<?php include 'src/footer.php'; ?>
+</footer>
+		
+<!-- Controle do pop-up de promoção -->
+<script>
+    // Função para abrir o pop-up
+    function openPopup() {
+        document.getElementById('promoPopup').style.display = 'flex';
+    }
+
+    // Função para fechar o pop-up
+    function closePopup() {
+        document.getElementById('promoPopup').style.display = 'none';
+    }
+
+    // Fecha o pop-up ao clicar fora do quadrante
+    document.addEventListener('click', function(event) {
+        const popup = document.getElementById('promoPopup');
+        const popupContent = document.querySelector('.popup-content');
+        
+        if (popup.style.display === 'flex' && !popupContent.contains(event.target)) {
+            closePopup();
+        }
+    });
+
+    // Abra o pop-up automaticamente após 1,5 segundos
+    window.onload = function() {
+        setTimeout(openPopup, 1500);
+    };
+</script>
+
+		
+		<!-- jquery Min JS -->
+        <script src="js/jquery.min.js"></script>
+		<!-- jquery Migrate JS -->
+		<script src="js/jquery-migrate-3.0.0.js"></script>
+		<!-- jquery Ui JS -->
+		<script src="js/jquery-ui.min.js"></script>
+		<!-- Easing JS -->
+        <script src="js/easing.js"></script>
+		<!-- Color JS -->
+		<script src="js/colors.js"></script>
+		<!-- Popper JS -->
+		<script src="js/popper.min.js"></script>
+		<!-- Bootstrap Datepicker JS -->
+		<script src="js/bootstrap-datepicker.js"></script>
+		<!-- Jquery Nav JS -->
+        <script src="js/jquery.nav.js"></script>
+		<!-- Slicknav JS -->
+		<script src="js/slicknav.min.js"></script>
+		<!-- ScrollUp JS -->
+        <script src="js/jquery.scrollUp.min.js"></script>
+		<!-- Niceselect JS -->
+		<script src="js/niceselect.js"></script>
+		<!-- Tilt Jquery JS -->
+		<script src="js/tilt.jquery.min.js"></script>
+		<!-- Owl Carousel JS -->
+        <script src="js/owl-carousel.js"></script>
+		<!-- counterup JS -->
+		<script src="js/jquery.counterup.min.js"></script>
+		<!-- Steller JS -->
+		<script src="js/steller.js"></script>
+		<!-- Wow JS -->
+		<script src="js/wow.min.js"></script>
+		<!-- Magnific Popup JS -->
+		<script src="js/jquery.magnific-popup.min.js"></script>
+		<!-- Counter Up CDN JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
+		<!-- Bootstrap JS -->
+		<script src="js/bootstrap.min.js"></script>
+		<!-- Main JS -->
+		<script src="js/main.js"></script>
+
+		
+		
+    </body>
+</html>
