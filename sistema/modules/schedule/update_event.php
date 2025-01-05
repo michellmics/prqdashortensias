7039f997bf47d39dcfdd9c9d22979b89a52526ee
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-05 23:48:24 +0000

    deploy

--- sistema/modules/schedule/update_event.php
+++ sistema/modules/schedule/update_event.php
@@ -3,6 +3,29 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+session_start(); 
+define('SESSION_TIMEOUT', 43200); // 30 minutos
+
+if (!isset($_SESSION['user_id'])) 
+{
+  header("Location: https://www.prqdashortensias.com.br/index.php");
+  exit();
+}
+    // Atualiza o timestamp da última atividade
+$_SESSION['last_activity'] = time();
+
+if (!isset($_SESSION['user_id'])) 
+{
+  header("Location: https://www.prqdashortensias.com.br/index.php");
+  exit();
+}
+
+$blocoSession = $_SESSION['user_bloco'];
+$apartamentoSession = $_SESSION['user_apartamento'];
+$nomeSession =  ucwords($_SESSION['user_name']);
+$usuariologado = $nomeSession." <b>BL</b> ".$blocoSession." <b>AP</b> ".$apartamentoSession;
+$userid = $_SESSION['user_id'];
+
 $data = json_decode(file_get_contents('php://input'), true);
 
 include_once '../../../objetos.php'; // Carrega a classe de conexão e objetos
