472c99ad18da1c258b00a438b5ca69087075202f
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-07 08:19:43 +0000

    deploy

--- sistema/view_relatorio.php
+++ sistema/view_relatorio.php
@@ -197,7 +197,7 @@ html, body {
 
 					
   		<!-- ini content Area -->
-		  <section class="content" style="display: flex; justify-content: center; align-items: center; height: 100vh; flex-direction: column; padding-top: 150px;">
+		  <section class="content" style="display: flex; justify-content: center; align-items: center; height: 150vh; flex-direction: column; padding-top: 180px;">
 	
 			<div class="container">
 			<div class="row">
